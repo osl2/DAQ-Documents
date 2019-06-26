06982f20867b69747efcf0d44e22e89aa31212af
JanKue <jan.kueblbeck@gmail.com>
2019-06-26 21:07:22 +0200

    added snakeyaml examples

---
+++ 2-Entwurf/Entwicklung/src/edu/kit/snakeyaml/examples/ParseYAML.java
@@ -0,0 +1,36 @@
+package edu.kit.snakeyaml.examples;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+public class ParseYAML {
+
+	public static void main(String[] args) throws IOException {
+		
+		String filePath = "resources/ADS1115Config.yaml";
+
+		Yaml yaml = new Yaml();
+		InputStream input = new FileInputStream(new File(filePath));
+		
+		Object obj = yaml.load(input);
+		System.out.println(obj.getClass()); // for PhyPiDAQ config files, this should be a LinkedHashMap
+		
+		Map map = (Map) obj;
+		System.out.println(map); // print entire content of the map
+		
+		System.out.println(map.get("DAQModule")); // print module name (example)
+
+	}
+
+}
