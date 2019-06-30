7a8d03ec99f4d3befbe785dad473ed49f6996445
JanKue <jan.kueblbeck@gmail.com>
2019-06-30 17:11:49 +0200

    undo+redo verbessert

--- 2-Entwurf/2-Entwicklung/src/edu/kit/freejdaq/controller/CommandManager.java
+++ 2-Entwurf/2-Entwicklung/src/edu/kit/freejdaq/controller/CommandManager.java
@@ -10,16 +10,38 @@ import java.util.Stack;
  */
 public class CommandManager {
 	
-	private Stack<Command> pastCommands;
+	private Stack<Command> doneCommands;
+	private Stack<Command> undoneCommands;
 	
-	public void doCmd(Command cmd) throws FileNotFoundException {
-		pastCommands.push(cmd);
+	/**
+	 * runs a specified command, adds it to the undo stack, and clears the redo stack
+	 * @param cmd Command object
+	 * @throws FileNotFoundException
+	 */
+	public void doCommand(Command cmd) throws FileNotFoundException {
 		cmd.execute();
+		doneCommands.push(cmd);
+		undoneCommands.clear();
 	}
 	
-	public void undo( ) throws FileNotFoundException {
-		Command cmd = pastCommands.pop();
+	/**
+	 * undoes the latest command, adds it to the redo stack
+	 * @throws FileNotFoundException
+	 */
+	public void undo() throws FileNotFoundException {
+		Command cmd = doneCommands.pop();
 		cmd.unExecute();
+		undoneCommands.push(cmd);
 	}
-
+	
+	/**
+	 * redoes the latest undone command, adds it back on the undo stack
+	 * @throws FileNotFoundException
+	 */
+	public void redo() throws FileNotFoundException {
+		Command cmd = undoneCommands.pop();
+		cmd.execute();
+		doneCommands.push(cmd);
+	}
+	
 }
