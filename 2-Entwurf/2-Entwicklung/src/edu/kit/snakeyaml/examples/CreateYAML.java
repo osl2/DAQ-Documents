e12ece455ce508417b139043a51696a2e2df6355
Leon Huck <huckleon@googlemail.com>
2019-08-11 02:53:52 +0200

    Implementierungsdokument weitergeschrieben.

--- 2-Entwurf/2-Entwicklung/src/edu/kit/snakeyaml/examples/CreateYAML.java
+++ 2-Entwurf/2-Entwicklung/src/edu/kit/snakeyaml/examples/CreateYAML.java
@@ -10,21 +10,21 @@ import org.yaml.snakeyaml.Yaml;
 
 public class CreateYAML {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		String filePath = "resources/dumpOutput.yaml";
-		Map map = new HashMap<String, String>();
-		
-		map.put("DAQModule", "ExampleModuleConfig");
-		map.put("exampleKey", "exampleEntry");
-		
-		Yaml yaml = new Yaml();
-		
-		OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(filePath));
-		yaml.dump(map, writer);
-		writer.close();
-		System.out.println("Success!");
+        String filePath = "resources/dumpOutput.yaml";
+        Map map = new HashMap<String, String>();
 
-	}
+        map.put("DAQModule", "ExampleModuleConfig");
+        map.put("exampleKey", "exampleEntry");
+
+        Yaml yaml = new Yaml();
+
+        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(filePath));
+        yaml.dump(map, writer);
+        writer.close();
+        System.out.println("Success!");
+
+    }
 
 }
